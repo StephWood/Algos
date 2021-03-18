23cc06007040096a83ea0a027f6f061dcbfefe1c
Stephanie Wood <wood.steph@gmail.com>
2021-03-18 11:27:36 -0700

    reverse words solution

--- reverse-words-in-string.js
+++ reverse-words-in-string.js
@@ -1,12 +1,17 @@
 function reverseWordsInString(string) {
   const reversedString = [];
+  let currString = '';
 
-  for (let i = 0; i < string.length; i++) {
-    let currString = '';
-    if (string[i] === ' ') {
+  for (let i = 0; i <= string.length; i++) {
+    if (string[i] === ' ' || i === string.length) {
       reversedString.unshift(currString);
       currString = '';
-    } else currString + string[i];
+    } else currString += string[i];
   }
   return reversedString.join(' ');
 }
+
+console.log(reverseWordsInString('Job hunting is the worst'));
+console.log(reverseWordsInString('I like cookies'));
+console.log(reverseWordsInString('Reverse these words'));
+console.log(reverseWordsInString('I am not a cat'));
